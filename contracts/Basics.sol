b4fb388b1ac8d2ab228c470c7f623202b1adb053
Hoodjie Kilong <misterfighter1990@gmail.com>
2025-03-09 10:15:19 -0700

    play with counters

--- contracts/Basics.sol
+++ contracts/Basics.sol
@@ -2,13 +2,33 @@
 pragma solidity ^0.8.28;
 
 contract Basics {
-    string public greetings;
+    // string public greetings = "Hello, World";
+    // string public name = "ABDURRAUF";
+    // string public surname = "Sakenov";
 
-    function setGreetings(string calldata message) external {
-        greetings = message;
-    }
+    // function getFullName() public view returns(string memory) {
+    //     return string(abi.encodePacked(name, surname));
+    // }
 
-    function getGreetings() public view returns (string memory) {
-		return greetings;
-	}
+    // function setGreetings(string calldata message) external {
+    //     greetings = message;
+    // }
+
+    // function getGreetings() public view returns (string memory) {
+	// 	return greetings;
+	// }
+
+    // uint256 public counter;
+
+    // function setCounter(uint256 _count) public {
+    //     counter = _count;
+    // }
+
+    // function increment() public {
+    //     counter++;
+    // }
+
+    // function decrement() public {
+    //     counter--;
+    // }
 }
